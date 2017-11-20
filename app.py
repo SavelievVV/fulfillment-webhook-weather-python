8db2c7f9d2e3b0328ddc42b3459b5f384243b146
SavelievVV <vladimir@saveliev.online>
2017-11-20 03:08:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,10 +106,10 @@ def makeWebhookResult(data):
 
     return {
         "speech": speech,
-        "displayText": speech,
+        "displayText": speech
         #"data": data,
         #"contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        #"source": "apiai-weather-webhook-sample"
     }
 
 
